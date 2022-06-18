cdccd6455a2a9abb2367f409359e8940096d22fb
ShubhamNautiyal21 <107478434+ShubhamNautiyal21@users.noreply.github.com>
2022-06-18 18:41:11 +0530

    Add files via upload

---
+++ MonoBehaviourSingleton.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This script is responsible to provide singleton pattern
+/// </summary>
+public abstract class MonoBehaviourSingleton<T> : MonoBehaviour
+{
+    private static Dictionary<Type, object> singletons = new Dictionary<Type, object>();
+    public static T Instance
+    {
+        get
+        {
+            return (T)singletons[typeof(T)];
+        }
+    }
+
+    private void Awake()
+    {
+
+        if (singletons.ContainsKey(GetType()))
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            singletons.Add(GetType(), this.gameObject);
+            DontDestroyOnLoad(this);
+        }
+    }
+    
+}
